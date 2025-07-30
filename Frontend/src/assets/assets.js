3184aba82e078cac61de44f96a4304444a0abb1a
Partha Maity <parthamaity2004@gmail.com>
2025-07-30 20:39:41 +0530

    All images are uploaded and assests folder was done

---
+++ Frontend/src/assets/assets.js
@@ -0,0 +1,49 @@
+import app_store from './app_store.png'
+import beauty from './beauty.png'
+import carpenter from './carpenter.png'
+import Cleaning from './Cleaning.png'
+import cooking from './cooking.png'
+import cross_icon from './cross_icon.png'
+import Driver from './Driver.png'
+import Electrician from './Electrician.png'
+import facebook_icon from './facebook_icon.png'
+import Laundry from './Laundry.png'
+import Linkedin_icon from './Linkedin_icon.png'
+import logout_icon from './logout_icon.png'
+import Painter from './Painter.png'
+import play_store from './play_store.png'
+import Plumber from './Plumber.png'
+import profile_icon from './profile_icon.png'
+import rating_starts from './rating_starts.png'
+import search_icon from './search_icon.png'
+import Shifting from './Shifting.png'
+import Technician from './Technician.png'
+import twitter_icon from './twitter_icon.png'
+import Welder from './Welder.png'
+
+
+
+export const assets = {
+    app_store,
+    beauty,
+    carpenter,
+    Cleaning,
+    cooking,
+    cross_icon,
+    Driver,
+    Electrician,
+    facebook_icon,
+    Laundry,
+    Linkedin_icon,
+    logout_icon,
+    Painter,
+    play_store,
+    Plumber,
+    profile_icon,
+    rating_starts,
+    search_icon,
+    Shifting,
+    Technician,
+    twitter_icon,
+    Welder
+}
